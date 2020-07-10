0fab0a896e7f8ee0247db617232abd687380b4d8
e <2223486807@qq.com>
2020-07-10 11:57:35 +0800

    小修改

--- merchant_service80/src/main/java/com/qtummatrix/controller/XD_UserController.java
+++ merchant_service80/src/main/java/com/qtummatrix/controller/XD_UserController.java
@@ -1,3 +1,15 @@
 package com.qtummatrix.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@DefaultProperties(defaultFallback = "defaultFallbackMethod")
 public class XD_UserController {
+
+
+
+    public String defaultFallbackMethod(){
+        return "网络异常，请稍后再试，/(ㄒoㄒ)/~~";
+    }
 }
--- merchant_service80/src/main/java/com/qtummatrix/service/XD_UserService.java
+++ merchant_service80/src/main/java/com/qtummatrix/service/XD_UserService.java
@@ -1,9 +1,11 @@
 package com.qtummatrix.service;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 
 @Component
 @FeignClient(value = "merchant-user" ,fallback = XD_UserServiceApplication.class)
+@Hystrix
 public interface XD_UserService {
 }
