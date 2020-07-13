cb7cd7e5481dd0dccc6982adaec1fdc21c2b2eea
e <2223486807@qq.com>
2020-07-13 11:33:25 +0800

    小修改

--- merchant_service9999/src/main/java/com/qtummatrix/controller/XD_UserController.java
+++ merchant_service9999/src/main/java/com/qtummatrix/controller/XD_UserController.java
@@ -7,9 +7,8 @@ import org.springframework.stereotype.Controller;
 @DefaultProperties(defaultFallback = "defaultFallbackMethod")
 public class XD_UserController {
 
-
-
     public String defaultFallbackMethod(){
         return "网络异常，请稍后再试，/(ㄒoㄒ)/~~";
     }
+
 }
